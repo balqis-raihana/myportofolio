7d9db905fc32cdd3bdec2aba87d69af7e8aaaf3f
Balqis <balqis.raihana@ui.ac.id>
2026-09-10 23:03:07 +0700

    feat: implement Coursework model, view, template, and tests for Tugas 2

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from main.models import Experience
+from main.models import Experience, Coursework
 
 
 def show_main(request):
@@ -23,3 +23,11 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+
+def show_coursework(request):
+    context = {
+        "name": "Balqis Raihana",
+        "coursework_list": Coursework.objects.all(),
+    }
+    return render(request, "coursework.html", context)
